015d76111fb3baa845fa52660e78b6fef28b213f
Jason Braid <braidj@gmail.com>
2021-03-13 15:59:43 +0000

    How to enable verbose mode

--- src/pydoc_tests.py
+++ src/pydoc_tests.py
@@ -2,7 +2,10 @@
 """This is an example module to demonstrate the capabilities of pydoc
 integrated with doctest
 Note that the result within the interactive section is not prefixed
-with >>"""
+with >>
+If no failures then there will be no output. If you need to see output then enable the 
+verbose mode via a -v e.g.
+python3 pydoc_tests.py -v"""
 
 
 def multiply_by_two(a):
